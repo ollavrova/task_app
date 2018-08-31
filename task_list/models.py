b1009ee0b9e4f55c4daf200d7b018efd2fc09d75
krocozabr <krocozabr@gmail.com>
2018-08-31 03:49:47 +0300

    added model and migrations

--- task_list/models.py
+++ task_list/models.py
@@ -1,3 +1,15 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Task(models.Model):
+    name = models.CharField(max_length=250)
+    description = models.TextField()
+    status = models.BooleanField(default=False, verbose_name='Is Task Done?')
+    created = models.DateTimeField(auto_now_add=True)
+    assigned = models.ForeignKey(User, models.SET_NULL, blank=True, null=True, related_name='+')
+    done_by = models.ForeignKey(User, models.SET_NULL, blank=True, null=True, related_name='+')
+    done_date = models.DateTimeField(blank=True, null=True)
+
+    def __str__(self):
+        return '{} {}'.format(str(self.id), self.name)
