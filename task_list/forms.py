cb0ee9ef04296f3a59b500c6f31401d55bcbd9ef
krocozabr <krocozabr@gmail.com>
2018-09-01 17:25:37 +0300

    fixed actions, added some refactoring

--- task_list/forms.py
+++ task_list/forms.py
@@ -1,17 +1,9 @@
 from django.forms import ModelForm
-
 from task_list.models import Task
 
 
-class TaskUpdateForm(ModelForm):
-    def __init__(self, *args, **kwargs):
-        self.user = kwargs.pop('user', None)
-        super(TaskUpdateForm, self).__init__(*args, **kwargs)
+class TaskCreateForm(ModelForm):
 
     class Meta:
         model = Task
         fields = ['name', 'description', 'status', 'assigned']
-
-
-
-
--- task_list/views.py
+++ task_list/views.py
@@ -1,8 +1,9 @@
+from django.http import HttpResponseRedirect
 from django.utils import timezone
 from django.views.generic import ListView, CreateView, UpdateView, DeleteView, DetailView
 from django.shortcuts import render_to_response
 from django.template import RequestContext
-from task_list.forms import TaskUpdateForm
+from task_list.forms import TaskCreateForm
 from task_list.models import Task
 
 
@@ -13,8 +14,8 @@ class TaskList(ListView):
 
 class TaskCreate(CreateView):
     model = Task
+    form_class = TaskCreateForm
     success_url = '/'
-    fields = ['name', 'description', 'status', 'assigned']
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
@@ -22,29 +23,53 @@ class TaskCreate(CreateView):
         context['button'] = 'Add'
         return super().get_context_data(**context)
 
+    def get_form(self, form_class=None):
+        return TaskCreateForm(initial={'assigned': self.request.user.pk})
+
     def post(self, request, *args, **kwargs):
-        obj = self.get_object()
-        form = self.get_form()
+        form = TaskCreateForm(request.POST)
         if form.is_valid():
-            if not obj.id:
-                obj.assigned = request.user
+            obj = form.save(commit=False)
             if 'status' in form.changed_data and form.cleaned_data['status'] is True:
                 obj.done_by = request.user
                 obj.done_date = timezone.now()
-                obj.save()
-            return self.form_valid(form)
-        return super().post(request, *args, **kwargs)
+            obj.save()
+            return HttpResponseRedirect(self.success_url)
+        else:
+            return self.form_invalid(form)
 
 
 class TaskDetail(DetailView):
     model = Task
     success_url = '/'
-    template_name = 'task_list/task_detail.html'
 
 
-class TaskUpdate(UpdateView):
+class PostMixin:
+    """
+    mixin to post method which will process writing and clear a user who mark task 'done'
+    """
+
+    def post(self, request, *args, **kwargs):
+        obj = self.get_object()
+        form = self.get_form()
+        if form.is_valid():
+            if 'status' in form.cleaned_data:
+                if form.cleaned_data['status'] is True:
+                    # will write a user who make mark done
+                    obj.done_by = request.user
+                    obj.done_date = timezone.now()
+                else:
+                    obj.done_by = None
+                    obj.done_date = None
+            obj.save()
+            return super().post(request, *args, **kwargs)
+        else:
+            return self.form_invalid(form)
+
+
+class TaskUpdate(PostMixin, UpdateView):
     model = Task
-    form_class = TaskUpdateForm
+    fields = ['name', 'description', 'status', ]
     success_url = '/'
     template_name = 'task_list/task_form.html'
 
@@ -57,34 +82,18 @@ class TaskUpdate(UpdateView):
     def get_queryset(self):  # only owner can edit task
         return self.model.objects.filter(assigned=self.request.user)
 
-    def post(self, request, *args, **kwargs):
-        obj = self.get_object()
-        form = self.get_form()
-        if form.is_valid():
-            if 'status' in form.changed_data and form.cleaned_data['status'] is True:
-                obj.done_by = request.user
-                obj.done_date = timezone.now()
-                obj.save()
-            return self.form_valid(form)
-        return super().post(request, *args, **kwargs)
 
-
-class TaskDone(UpdateView):
+class TaskDone(PostMixin, UpdateView):
     model = Task
-    template_name = 'task_list/make_done.html'
-    fields = ['status']
+    template_name = 'task_list/task_form.html'
+    fields = ['status', ]
     success_url = '/'
 
-    def post(self, request, *args, **kwargs):
-        obj = self.get_object()
-        form = self.get_form()
-        if form.is_valid():
-            if 'status' in form.changed_data and form.cleaned_data['status'] is True:
-                obj.done_by = request.user
-                obj.done_date = timezone.now()
-                obj.save()
-            return self.form_valid(form)
-        return super().post(request, *args, **kwargs)
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Mark task done '
+        context['button'] = 'Save'
+        return super().get_context_data(**context)
 
 
 class TaskDelete(DeleteView):
